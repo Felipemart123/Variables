d83f7ff3faec1cda0e3ba542e8549f00e8af6f4f
Felipemart123 <felipemart1876@gmail.com>
2022-07-01 12:28:36 -0400

    created variables

---
+++ Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+string name = "bob";
+int x = 10;
+char a = 'b';
+bool isCSharpFun = true;
+double myNumber = 10.5;
+double n = 12.3;
+
+Console.WriteLine($"my name is {name} i am {x} years old");
+Console.WriteLine($" is this {isCSharpFun}");
+Console.WriteLine($"this number {myNumber} is smaller than {n}");
+
+
+
